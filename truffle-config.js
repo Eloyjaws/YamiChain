a0445350e7884d21ba2f3b2589f7871c68a808b2
Eniola Ajiboye <eloyjaws@gmail.com>
2021-02-15 15:37:10 +0200

    Initial commit, scaffold

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+       host: "127.0.0.1",
+       port: 7545, // Connect with the local blockchain: ganache
+       // port: 8545
+       network_id: "*",
+    }
+  },
+  compilers: {
+    solc: {
+      settings: {   
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      }
+    }
+  }
+};
